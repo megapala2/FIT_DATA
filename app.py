7c0e0fbda51b07e219f1fd2ea5e863961eef9c40
Thales Rudolph <rudolphthales1@gmail.com>
2024-03-22 15:53:43 -0300

    A lot of changes: * Added a plotchart; * Cleaned the data better * Better filters and layout * Added more data

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 import ast
 import plotly.express as px
 import json
+from datetime import datetime
 
 #   PT:
 #   Essa função serve para limpar os dados, o dado em CSV vem com uma coluna "Value" onde possui um dicionário DENTRO da coluna, 
@@ -36,7 +37,8 @@ def clean_key_data(df_health, valor: str):
     
 
     if 'time' in df_health.columns:
-        df_health['time'] = pd.to_datetime(df_health['time'], unit='s', errors='coerce')
+        df_health = df_health.drop('time', axis=1)
+        json_df = json_df.drop('time', axis=1)
     
     if 'date_time' in df_health.columns:
        df_health['date_time'] = pd.to_datetime(df_health['date_time'], unit='s', errors='coerce')
@@ -52,26 +54,37 @@ def clean_key_data(df_health, valor: str):
 
     if 'wake_up_time' in df_health.columns:
         df_health['wake_up_time'] = pd.to_datetime(df_health['wake_up_time'], unit='s', errors='coerce')
-
+    
    
     return df_health, json_df.columns.to_list()
 
 def clean_date(df_health):
     
+    df_health['Time'] = pd.to_datetime(df_health['Time'], unit='s', errors='coerce')
+    df_health['Date'] = df_health['Time'].dt.strftime('%d-%m-%Y')
+    df_health["Hora"] = df_health["Time"].dt.strftime('%H:%M')
+    
+  
     
 
-    df_health['UpdateTime'] = pd.to_datetime(df_health['UpdateTime'], unit='s', errors='coerce')
-    df_health['Time'] = pd.to_datetime(df_health['Time'], unit='s', errors='coerce')
-    df_health["Mês-Ano"] = df_health["UpdateTime"].apply(lambda x: str(x.day)+ "-" + str(x.month) + "-" + str(x.year))
-    df_health = df_health.drop('Uid', axis=1)
     df_health = df_health.drop('Sid', axis=1)
+    df_health = df_health.drop('Time', axis=1)
+    df_health = df_health.drop('UpdateTime', axis=1)
+    df_health = df_health.drop('Uid', axis=1)
 
     return df_health
 
+def date_filter(df_health, startDate, endDate):
+
+    df_health['Time'] = pd.to_datetime(df_health['Time'], unit='s', errors='coerce')
+    df_health = df_health[( df_health['Time'].dt.date >= startDate) & ( df_health['Time'].dt.date <= endDate)]
+    
+    return df_health
+   
 def skill_chart(df, valor: str, cor: str):
  
     fig = px.bar(
-        x=df['Time'], 
+        x=df['Date'], 
         y=df[valor], 
         color=df[cor],
         orientation='v',
@@ -85,24 +98,52 @@ def skill_chart(df, valor: str, cor: str):
 
 
 st.set_page_config(layout='wide')
+st.title('MIBAND DATA')
+st.info('If you want to find your Mifit data go to: https://www.mi.com/global/support/article/KA-11566/')
+
 col1, col2, col3 = st.columns(3)
+sideCol1, sideCol2 = st.sidebar.columns(2)
+
 
 df = pd.read_csv(f'{os.getcwd()}\\DADOS\\HEALTH_DATA.csv')
+
+
+
+filter_data_I = sideCol1.date_input(
+            'Start Date', value=datetime(2019, 1, 1), format="DD/MM/YYYY", key='dataInicial')
+filter_data_F = sideCol2.date_input(
+            'End Date', value="today", format="DD/MM/YYYY", key='dataFinal')
+
+
+
+df = date_filter(df, filter_data_I, filter_data_F )
+
 df_health = clean_date(df) 
+
+
+
+
 
 categorias = df_health['Key'].unique()
 
-selecionado = col1.selectbox(label="Selecione a categoria que deseja olhar!", options=categorias)
+
+
+selecionado = col1.selectbox(label="Select the category you wish to analyse!", options=categorias)
+
 
 
 df_steps, cols = clean_key_data(df_health, valor=selecionado)
 
+
+
+
 if selecionado:
-    valores = col2.selectbox(label='Selecione a coluna de valor!', options=cols)
-    cor = col3.selectbox(label='Selecione a coluna de cor!', options=cols)
+    valores = col2.selectbox(label='Select the value column!', options=cols)
+    cor = col3.selectbox(label='Select the color column!', options=cols)
 
 with st.expander('DataFrame'):
     st.info(f'Rows of data: {len(df_steps)}')
     st.dataframe(df_steps, use_container_width=True)
 
-
+fig = skill_chart(df_steps, valor=valores, cor=cor)
+st.plotly_chart(fig, use_container_width=True)
