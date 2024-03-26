58e971c85c410b3397d5d58e29741d922cc44b03
Thales Rudolph <rudolphthales1@gmail.com>
2024-03-26 18:51:48 -0300

    Did lot of changes, most of the site now is in PT-BR, all the dataframes work better

--- app.py
+++ app.py
@@ -6,6 +6,67 @@ import plotly.express as px
 import json
 from datetime import datetime
 
+
+EXPLANATION = {"weight": "Peso",
+               "bmi": "IMC",
+               "body_fat_rate": r"% Gordura",
+               "Date": "Data",
+               "moisture_rate": "% Hidratação",
+               "bone_mass": "% Massa Óssea",
+               "basal_metabolism": "Metabolismo Basal",
+               "muscle_rate": "% Massa Muscular",
+               "protein_rate": "% Proteína",
+               "visceral_fat": r"% Gordura Visceral",
+               "bpm": "BPM",
+               "stress": "Estresse",
+               "calories": "Calorias",
+               "spo2": "% Oxigênio no Sangue",
+               "bedtime": "H. Adormeceu",
+               "wake_up_time": "H. Acordou",
+               "timezone": "Fuso horário",
+               "duration": "Duração (minutos)",
+               "sleep_deep_duration": "Sono Profundo (minutos)",
+               "sleep_light_duration": "Sono Leve (minutos)",
+               "sleep_rem_duration": "Sono Rem (minutos)",
+               "sleep_awake_duration": "Tempo acordado (minutos)",
+               "total_score": "Pontuação",
+               "breath_quality": "Qualidade da respiração",
+               "daily_pai": "Diário",
+               "total_pai": "Total",
+               "high_zone_pai": "Máximo",
+               "medium_zone_pai": "Media",
+               "low_zone_pai": "Baixa",
+               "start_time": "Inicio",
+               "end_time": "Fim",
+               "type": "Tipo",
+               "steps": "Passos",
+               "distance": "Distância",
+
+}
+
+CATEGORIAS = {"weight": "Peso",
+              "heart_rate": "Batimentos",
+              "stress": "Estresse",
+              "steps": "Passos",
+              "calories": "Calorias",
+              "single_spo2": "Somente Oxigênio no Sangue",
+              "watch_night_sleep": "Sono",
+              "pai": "PAI",
+              "resting_heart_rate": "Batimentos em Repouso",
+              "dynamic": "Dinâmico",
+              "intensity": "Intenso",
+              "single_heart_rate": "Somente Batimentos",
+              "watch_daytime_sleep": "Cochilos",
+              "single_stress": "Somente Estresse",
+              "abnormal_heart_beat": "Batimentos Anormais",
+
+}
+
+VALORES = ["Data", "Hora", "Peso", "IMC", r"% Gordura", "% Hidratação", "% Massa Óssea", "Metabolismo Basal", 
+           "% Massa Muscular", "% Proteína", r"% Gordura Visceral", "BPM", "Estresse", "Calorias", ""
+           
+           
+           ] 
 #   PT:
 #   Essa função serve para limpar os dados, o dado em CSV vem com uma coluna "Value" onde possui um json DENTRO da coluna, 
 #   entretanto, esse json muda de acordo com o valor de "Key". Essa função vai limpar colunas dispensáveis para a leitura dos dados, arrumar a data,
@@ -17,13 +78,11 @@ from datetime import datetime
 #   this json varies according to the value of "Key". This function will clean unnecessary columns for data reading, 
 #   fix the date, extract each value from the "Value" json, and create a new column in the DataFrame.
 
-
 def clean_key_data(df_health, valor: str):
 
    
     df_health = df_health[df_health['Key'] == valor].copy()
     df_health['parsed_json'] = df_health['Value'].apply(json.loads)
- 
  
     
     json_df = pd.json_normalize(df_health['parsed_json'])
@@ -33,6 +92,7 @@ def clean_key_data(df_health, valor: str):
 
     df_health= df_health.drop('Value', axis=1)
     df_health= df_health.drop('parsed_json', axis=1)
+    df_health= df_health.drop('Key', axis=1)
     
     
 
@@ -41,7 +101,16 @@ def clean_key_data(df_health, valor: str):
         json_df = json_df.drop('time', axis=1)
     
     if 'date_time' in df_health.columns:
-       df_health['date_time'] = pd.to_datetime(df_health['date_time'], unit='s', errors='coerce')
+       df_health = df_health.drop('date_time', axis=1)
+       json_df = json_df.drop('date_time', axis=1)
+    
+    if 'items' in df_health.columns:
+       df_health = df_health.drop('items', axis=1)
+       json_df = json_df.drop('items', axis=1)
+
+    if 'friendly_score' in df_health.columns:
+       df_health = df_health.drop('friendly_score', axis=1)
+       json_df = json_df.drop('friendly_score', axis=1)
     
     if 'start_time' in df_health.columns:
         df_health['start_time'] = pd.to_datetime(df_health['start_time'], unit='s', errors='coerce')
@@ -54,13 +123,23 @@ def clean_key_data(df_health, valor: str):
 
     if 'wake_up_time' in df_health.columns:
         df_health['wake_up_time'] = pd.to_datetime(df_health['wake_up_time'], unit='s', errors='coerce')
+    
+    
 
     df_health = df_health.dropna()
     df_health = df_health[(df_health.iloc[:, 1:] != 0).all(axis=1)]
+    df_health = df_health.rename(columns=EXPLANATION)
+    json_df = json_df.rename(columns=EXPLANATION)
     
-   
     return df_health, json_df.columns.to_list()
 
+#   PT:
+#   Essa é a limpeza geral do dataframe, ele troca os padrões de dados para os padrões corretos, formata as datas corretamente, também dropa diversas colunas indesejadas
+
+#   EN:
+#   This is the general cleaning of the dataframe, it changes data patterns to the correct ones, formats dates properly, and also drops various unwanted columns. 
+
+@st.cache_data
 def clean_date(df_health):
     
     df_health['Time'] = pd.to_datetime(df_health['Time'], unit='s', errors='coerce')
@@ -74,8 +153,15 @@ def clean_date(df_health):
     df_health = df_health.drop('Time', axis=1)
     df_health = df_health.drop('UpdateTime', axis=1)
     df_health = df_health.drop('Uid', axis=1)
+    df_health['Key'] = df_health['Key'].replace(CATEGORIAS)
 
     return df_health
+
+#   PT:
+#   Esse é o filtro de data do dataframe
+
+#   EN:
+#   This is the date filter of the DF
 
 def date_filter(df_health, startDate, endDate):
 
@@ -83,28 +169,30 @@ def date_filter(df_health, startDate, endDate):
     df_health = df_health[( df_health['Time'].dt.date >= startDate) & ( df_health['Time'].dt.date <= endDate)]
     
     return df_health
-   
-def skill_chart(df, valor: str, cor: str):
 
-    
-    fig = px.bar(
-        x=df['Date'], 
-        y=df[valor], 
-        text=df[valor],
-        color=df[cor],
-        orientation='v',
+#   PT:
+#   Cria o gráfico
+
+#   EN:
+#   Create the chart
+   
+def skill_chart(df, valor: str, valor2: str):
+
+    fig = px.line(
+        df, 
+        x=valor, 
+        y=valor2, 
         template='presentation',
-        
     )
 
+
     return fig
-                        
    
 
 
 st.set_page_config(layout='wide')
 st.title('MIBAND DATA')
-st.info('If you want to find your Mifit data go to: https://www.mi.com/global/support/article/KA-11566/')
+st.info('Se quiser baixar os seus dados da MIBAND: https://www.mi.com/global/support/article/KA-11566/')
 
 col1, col2, col3 = st.columns(3)
 sideCol1, sideCol2 = st.sidebar.columns(2)
@@ -120,36 +208,42 @@ filter_data_F = sideCol2.date_input(
             'End Date', value="today", format="DD/MM/YYYY", key='dataFinal')
 
 
-
-df = date_filter(df, filter_data_I, filter_data_F )
-
 df_health = clean_date(df) 
 
-
+if filter_data_F:
+ df = date_filter(df, filter_data_I, filter_data_F )
 
 
 
 categorias = df_health['Key'].unique()
 
-
-
-selecionado = col1.selectbox(label="Select the category you wish to analyse!", options=categorias)
-
+selecionado = col1.selectbox(label="Selecione a categoria que deseja analisar!", options=categorias)
 
 
 df_steps, cols = clean_key_data(df_health, valor=selecionado)
+cols.append("Data")
+cols.append("Hora")
 
 
-
-
-if selecionado:
-    valores = col2.selectbox(label='Select the value column!', options=cols)
-    cor = col3.selectbox(label='Select the color column!', options=cols)
-
-with st.expander('DataFrame'):
-    st.info(f'Rows of data: {len(df_steps)}')
+with st.expander('DataFrame Tratado'):
+    st.info(f'Linhas do dataframe: {len(df_steps)}      |        Filtro: {selecionado}')
     st.dataframe(df_steps, use_container_width=True)
+with st.expander('DataFrame Semi Tratado'):
+    st.info(f'Linhas do dataframe: {len(df_health)}')
+    st.dataframe(df_health, use_container_width=True)
+with st.expander('DataFrame Bruto'):
+    st.info(f'Linhas do dataframe: {len(df)}')
+    st.dataframe(df, use_container_width=True)
+
+
+
+valores = col2.selectbox(label='Selecione o valor que deseja observar!', options=cols, index=None)
+cor = col3.selectbox(label='Selecione a cor da coluna!', options=cols, index=None)
+
+if valores and cor:
+
+    fig = skill_chart(df_steps, valor=valores, valor2=cor)
+    st.plotly_chart(fig, use_container_width=True)
+
     
 
-fig = skill_chart(df_steps, valor=valores, cor=cor)
-st.plotly_chart(fig, use_container_width=True)
